cb72710aeb495b54f9c8d94a6d0d89bd91308c10
shaad1133 <86961466+shaad1133@users.noreply.github.com>
2022-01-01 19:40:46 +0530

    Create nth_Ugly_Number.cpp

---
+++ Goldman Sachs/nth_Ugly_Number.cpp
@@ -0,0 +1,40 @@
+ull getNthUglyNo(int n)  {
+  
+	  ull ugly[n]; 
+    ugly[0] = 1;
+  
+    int i2 = 0, i3 = 0, i5 = 0;
+  
+    ull next_multiple_of_2 = 2;
+    ull next_multiple_of_3 = 3;
+    ull next_multiple_of_5 = 5;
+    ull next_ugly_no = 1;
+ 
+   
+    for (int i = 1; i < n; i++)  {
+      
+        next_ugly_no = min(next_multiple_of_2, min(next_multiple_of_3, next_multiple_of_5));
+      
+        ugly[i] = next_ugly_no;
+      
+        if (next_ugly_no == next_multiple_of_2) {
+          
+            i2 = i2 + 1;
+            next_multiple_of_2 = ugly[i2] * 2;
+        }
+        if (next_ugly_no == next_multiple_of_3) {
+          
+            i3 = i3 + 1;
+            next_multiple_of_3 = ugly[i3] * 3;
+        }
+        if (next_ugly_no == next_multiple_of_5) {
+          
+            i5 = i5 + 1;
+            next_multiple_of_5 = ugly[i5] * 5;
+        }
+    }  
+ 
+  
+    return next_ugly_no;
+  
+	}
