3041d5f36f47ce19ac26254295d0ab86ca0c6573
shaad1133 <86961466+shaad1133@users.noreply.github.com>
2022-01-01 18:19:42 +0530

    Update group_Anagrams.cpp

--- Goldman Sachs/group_Anagrams.cpp
+++ Goldman Sachs/group_Anagrams.cpp
@@ -1,1 +1,32 @@
 
+/* 
+Given an array of strings, return all groups of strings that are anagrams. 
+The groups must be created in order of their appearance in the original array. 
+*/
+
+vector<vector<string>>Anagrams(vector<string>& list) {
+          
+        map<map<char,int>,vector<string>>bm; 
+        
+        for(int i=0;i<list.size();i++) {
+            
+            map<char,int>sm; 
+            
+            for(auto c:list[i])
+                sm[c]++;
+            
+            bm[sm].push_back(list[i]);
+            
+        }
+        
+        vector<vector<string>>ans;
+        
+        for(auto i:bm) {
+            
+            ans.push_back(i.second);
+            
+        }
+        
+        return ans;
+        
+ }
